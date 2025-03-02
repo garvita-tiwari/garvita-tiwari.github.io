ea534c1d463d18cf2aef08b5d940784ac823741c
garvita-tiwari <garvita.tiwari9@gmail.com>
2025-03-02 16:13:08 +0000

    Deploying to gh-pages from @ garvita-tiwari/garvita-tiwari.github.io@f8a1345bd7ba188ec1aae0eb3579d2b16601ce34 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -493,18 +493,25 @@ ninja.data = [{
           window.open("mailto:%67%74%69%77%61%72%69@%6D%70%69-%69%6E%66.%6D%70%67.%64%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/garvita-tiwari", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/garvita-tiwari-804a0278", "_blank");
+        },
+      },{
+        id: 'social-medium',
+        title: 'Medium',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://medium.com/@garvita.tiwari9", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -514,11 +521,11 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=2P2CMukAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://garvita-tiwari.github.io/", "_blank");
+          window.open("https://twitter.com/_garvita_tiwari", "_blank");
         },
       },{
       id: 'light-theme',
